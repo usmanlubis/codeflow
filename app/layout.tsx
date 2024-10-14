876345c33dbc5ea3a10f42cb86f3ea90b6539c96
Usman Lubis <usmanlubis11@gmail.com>
2024-10-14 13:20:22 +0700

    feat: create theme context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@/context/Theme Provider';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk as SpaceGrotesk } from 'next/font/google';
@@ -31,17 +32,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<ClerkProvider
-			appearance={{
-				elements: {
-					formButtonPrimary: 'primary-gradient',
-					footerActionLink: 'primary-text-gradient hover:text-primary-500',
-				},
-			}}
-		>
-			<html lang="en">
-				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>{children}</body>
-			</html>
-		</ClerkProvider>
+		<html lang="en">
+			<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+				<ClerkProvider
+					appearance={{
+						elements: {
+							formButtonPrimary: 'primary-gradient',
+							footerActionLink: 'primary-text-gradient hover:text-primary-500',
+						},
+					}}
+				>
+					<ThemeProvider>{children}</ThemeProvider>
+				</ClerkProvider>
+			</body>
+		</html>
 	);
 }
