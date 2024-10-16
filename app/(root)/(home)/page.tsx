9f8b8d1feb523f573b64cbd8b39c1f3690b78a26
Usman Lubis <usmanlubis11@gmail.com>
2024-10-16 11:07:16 +0700

    feat: add navbar logo and avatar user

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,14 +1,3 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
-
 export default function Home() {
-	return (
-		<main>
-			<SignedOut>
-				<SignInButton />
-			</SignedOut>
-			<SignedIn>
-				<UserButton />
-			</SignedIn>
-		</main>
-	);
+	return <main></main>;
 }
---
+++ components/shared/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import { SignedIn, UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Navbar() {
+	return (
+		<nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+			<Link href="/" className="flex items-start gap-1">
+				<Image src=" /assets/images/site-logo.svg" height={23} width={23} alt="CodeFlow logo" />
+				<p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+					Dev <span className="text-primary-500">Overflow</span>
+				</p>
+			</Link>
+			GlobalTheme
+			<div className="flex-between gap-5">
+				<SignedIn>
+					<UserButton
+						appearance={{
+							elements: {
+								avatarBox: 'h-10 w-10',
+							},
+							variables: {
+								colorPrimary: '#FF7000',
+							},
+						}}
+					/>
+				</SignedIn>
+				Theme
+			</div>
+		</nav>
+	);
+}
