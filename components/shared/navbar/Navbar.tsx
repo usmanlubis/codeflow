f7823f3050e8f2ccc6d53c0f5d12c521b72f4540
Usman Lubis <usmanlubis11@gmail.com>
2024-10-17 08:17:36 +0700

    style: fix flex item alignment to center

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -1,12 +1,13 @@
 import { SignedIn, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import MobileNav from './MobileNav';
 import Theme from './Theme';
 
 export default function Navbar() {
 	return (
 		<nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
-			<Link href="/" className="flex items-start gap-1">
+			<Link href="/" className="flex items-center gap-1">
 				<Image src=" /assets/images/site-logo.svg" height={23} width={23} alt="CodeFlow logo" />
 				<p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
 					Dev <span className="text-primary-500">Overflow</span>
@@ -27,7 +28,7 @@ export default function Navbar() {
 						}}
 					/>
 				</SignedIn>
-				MobileNav
+				<MobileNav />
 			</div>
 		</nav>
 	);
